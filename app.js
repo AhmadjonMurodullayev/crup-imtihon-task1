adfb167a27fcbdc9fbda25d45f84240e73229aab
AhmadjonMurodullayev <murodullayevahmadjon26@gmail.com>
2024-08-19 03:30:38 +0500

    js

--- app.js
+++ app.js
@@ -6,11 +6,24 @@ const close = document.getElementById("close");
 let form = {};
 let products = [];
 let currentEditId = null;
+let page = 1;
+let prodactPages = 2;
+
 let bestUrl = "http://localhost:3000/Prodact";
 document.addEventListener("DOMContentLoaded", function () {
   open_moda.addEventListener("click", openMadal);
   save.addEventListener("click", saveProduct);
-  getProducts()
+  document.getElementById("prev").addEventListener("click", function () {
+    if (page !== 1) {
+      page--;
+      getProducts();
+    }
+  });
+  document.getElementById("next").addEventListener("click", function () {
+    page++;
+    getProducts();
+  });
+  getProducts();
 });
 function openMadal() {
   toggelModal("block");
@@ -29,53 +42,34 @@ window.addEventListener("click", function (event) {
     toggelModal("none");
   }
 });
-
-// async function saveProdact() {
-//   try {
-//     const response = await fetch(`${bestUrl}`, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(form),
-//     });
-//     console.log(response);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-
 async function saveProduct() {
-    try {
-      if (currentEditId) {
-        // Yangilanishi kerak bo'lgan mahsulot
-        const response = await fetch(`${bestUrl}/${currentEditId}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(form),
-        });
-        console.log(response);
-      } else {
-        // Yangi mahsulot qo'shish
-        const response = await fetch(`${bestUrl}`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(form),
-        });
-        console.log(response);
-      }
-      getProducts(); // Mahsulotlar ro'yxatini yangilash
-      toggelModal("none");
-    } catch (error) {
-      console.log(error);
+  try {
+    if (currentEditId) {
+      const response = await fetch(`${bestUrl}/${currentEditId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+      console.log(response);
+    } else {
+      const response = await fetch(`${bestUrl}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+      console.log(response);
     }
+    getProducts();
+    toggelModal("none");
+  } catch (error) {
+    console.log(error);
   }
-  
+}
+
 async function getProducts() {
   try {
     const response = await fetch(`${bestUrl}`);
@@ -88,9 +82,13 @@ async function getProducts() {
 
 function displayProduct() {
   result.innerHTML = "";
-  products.forEach((item, index) => {
+  const start = (page - 1) * prodactPages;
+  const end = start + prodactPages;
+  const paginationControls = products.slice(start, end);
+
+  paginationControls.forEach((item, index) => {
     console.log(item);
-    
+
     let tr = document.createElement("tr");
     tr.innerHTML = `
         <td>${index + 1}</td>
@@ -100,39 +98,91 @@ function displayProduct() {
         <td>${item.number}</td>
         <td>${item.color}</td>
         <td>
-        <button class="btn btn-info mx-1" onclick="editProduct('${item.id}')"> <i class="fa-solid fa-pen"></i> </button>
-        <button class="btn btn-danger" onclick="deleteProduct('${item.id}')"> <i class="fa-regular fa-trash-can"></i> </button>
+        <button class="btn btn-info mx-1" onclick="editProduct('${
+          item.id
+        }')"> <i class="fa-solid fa-pen"></i> </button>
+        <button class="btn btn-danger" onclick="deleteProduct('${
+          item.id
+        }')"> <i class="fa-regular fa-trash-can"></i> </button>
         </td>
         `;
-        result.appendChild(tr)
+    result.appendChild(tr);
   });
+  paginationUsers(filtered_users.length);
 }
 
-async function deleteProduct(id){
-    try{
-            await fetch(`${bestUrl}/${id}`,{
-                method: "DELETE"
-            })
-    }catch(error){
-        console.log(error);
-        
-    }
-}
-
-
-
-
-function editProduct(id) {
-    currentEditId = id;
-    const product = products.find(p => p.id === id);
-    if (product) {
-      form = { ...product };
-      document.querySelector("input[name='name']").value = product.name || '';
-      document.querySelector("input[name='price']").value = product.price || '';
-      document.querySelector("select[name='brand']").value = product.brand || '';
-      document.querySelector("input[name='number']").value = product.number || '';
-      document.querySelector("input[name='color']").value = product.color || '';
-      toggelModal("block");
-    }
+async function deleteProduct(id) {
+  try {
+    await fetch(`${bestUrl}/${id}`, {
+      method: "DELETE",
+    });
+  } catch (error) {
+    console.log(error);
   }
+}
+function editProduct(id) {
+  currentEditId = id;
+  const product = products.find((p) => p.id === id);
+  if (product) {
+    form = { ...product };
+    document.querySelector("input[name='name']").value = product.name || "";
+    document.querySelector("input[name='price']").value = product.price || "";
+    document.querySelector("select[name='brand']").value = product.brand || "";
+    document.querySelector("input[name='number']").value = product.number || "";
+    document.querySelector("input[name='color']").value = product.color || "";
+    toggelModal("block");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
